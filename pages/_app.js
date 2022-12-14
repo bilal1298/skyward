cd18e0d267c720dc8d1e4b401cf2f6381db7b709
Bilal Shaikh <bilalshaikh1298@gmail.com>
2022-12-14 12:24:03 +0530

    Changed page transition 5

--- pages/_app.js
+++ pages/_app.js
@@ -5,6 +5,25 @@ import Layout from "../components/UI/Layout";
 import { motion, AnimatePresence } from "framer-motion";
 import { useRouter } from "next/router";
 import Head from "next/head";
+import Router from "next/router";
+
+const routeChange = () => {
+  // Temporary fix to avoid flash of unstyled content
+  // during route transitions. Keep an eye on this
+  // issue and remove this code when resolved:
+  // https://github.com/vercel/next.js/issues/17464
+
+  const tempFix = () => {
+    const allStyleElems = document.querySelectorAll('style[media="x"]');
+    allStyleElems.forEach((elem) => {
+      elem.removeAttribute("media");
+    });
+  };
+  tempFix();
+};
+
+Router.events.on("routeChangeComplete", routeChange);
+Router.events.on("routeChangeStart", routeChange);
 
 function MyApp({ Component, pageProps }) {
   const [offset, setOffset] = useState(0);
