8aa297d48c91719ac412759054f35e48485225fa
Bilal Shaikh <bilalshaikh1298@gmail.com>
2022-12-28 23:27:20 +0530

    Added White Label Service Pages

--- pages/white-label-google-ads.jsx
+++ pages/white-label-google-ads.jsx
@@ -1,66 +1,57 @@
 import React from "react";
 import classes from "../styles/WhiteLabelServices.module.css";
 import Section from "../components/UI/Section";
-import Boxes from "../components/UI/Boxes";
 import Button from "../components/UI/Button";
 import Image from "next/image";
 import NumberStrip from "../components/ServicesPage/NumberStrip";
+import meta from "../public/meta.png";
+import google from "../public/google.png";
+import microsoft from "../public/microsoft.png";
+import tiktok from "../public/tiktok.png";
 import australia from "../public/australia.png";
 import seamless from "../public/seamlessCommunication.png";
 import nohogwash from "../public/nohogwash.png";
 import strategy from "../public/strategy.png";
 import wlweb from "../public/wlweb.png";
 import Pricing from "../components/UI/Pricing";
+import Boxes from "../components/UI/Boxes";
 import astronaut from "../public/astronaut.png";
 import Testimonials from "../components/HomePage/Testimonials";
 import CaseStudies from "../components/HomePage/CaseStudies";
-import goodfirms from "../public/goodfirms.png";
-import trustpilot from "../public/trustpilot.png";
-import clutch from "../public/clutchReview.png";
 
-const whitelabelcontent = () => {
+const whitelabelgoogle = () => {
   const submitHandler = (e) => {
     e.preventDefault();
   };
   const flipboxItems = [
     {
-      title: "Website Content",
-      desc: "The words your readers are reading should compel them to act! Our content is catchy. We help land readers, revenue and return on your investment. ",
+      title: "Fast Onboarding",
+      desc: "You’re only a click away from the fastest onboarding process in the industry. We go from signup and research to custom landing page designs and campaign launch within 2-3 days.",
       icon: astronaut,
     },
     {
-      title: "Blog Writing",
-      desc: "Whatever blog topic, we will help you write original and well-researched content. Your readers want to read engaging material and we help you write that. ",
+      title: "Campaign Management",
+      desc: "We simply link our Google Ads Manager Account to yours (takes 4 mins to do) and we’re ready to go. This means that your clients never find out that we’re managing their ads.",
+      icon: astronaut,
+    },
+    {
+      title: "Campaign Optimisations",
+      desc: "To ensure that your clients’ ads are only served to the right audience, we optimize all campaigns on a DAILY BASIS. Yes. You read that right. We have zero tolerance for irrelevant clicks.",
+      icon: astronaut,
+    },
+    {
+      title: "Tracking",
+      desc: "Our team handles setting up all of the conversion tracking and call tracking for you. We track everything from keywords and landing page visits to calls and contact forms so that performance metrics are always accurate to the T.",
       icon: astronaut,
     },
     {
       title: "Landing Pages",
-      desc: "Create a powerful and strategic landing page that will influence your audience to act! Let them help drive your revenue and the metrics you desire. ",
+      desc: "More often than not, client websites aren’t conversion optimized and they’re averse to the idea of making changes. For a ridiculously small fee (you can up-charge your client for a neat profit), we’ll create a highly converting landing page to increase your leads.",
       icon: astronaut,
     },
     {
-      title: "Resume Writing",
-      desc: "We help you write resumes that potential employers will make sure to notice and read. Let us amplify your skills to translate to potential employment.",
-      icon: astronaut,
-    },
-    {
-      title: "Branding & Advertising",
-      desc: " We will forge a brand identity that encapsulates you and is unique. Our honed marketing expertise will also propel your brand or business skyward!",
-      icon: astronaut,
-    },
-    {
-      title: "Email Promotions",
-      desc: "Email promotions that will stand out in a cluttered inbox. We create email content that will be catchy, and attractive, and drive traffic to you.",
-      icon: astronaut,
-    },
-    {
-      title: "Newsletters",
-      desc: "Do you want newsletters that your readers will love being subscribed to? Do you want newsletters that are fresh? We can do that for you.",
-      icon: astronaut,
-    },
-    {
-      title: "Press Releases",
-      desc: "Press releases require expertise that will allow them to be successful. We have the industry experience to help you launch engaging and well-executed press releases. ",
+      title: "Reporting",
+      desc: "In addition to our easy-to-understand dashboard that’s always at your disposal, we create custom weekly/monthly reports for your clients with YOUR BRANDING. The report will contain all core KPI stats and also compare to the previous period’s report.",
       icon: astronaut,
     },
   ];
@@ -69,16 +60,20 @@ const whitelabelcontent = () => {
       <section className={classes.whiteLabelBanner}>
         <div className={`container-lg ${classes.heroContainer}`}>
           <div className={classes.heroText}>
-            <h1 className="title">Premium White Label Content Partner</h1>
+            <h1 className="title">
+              <span className="titleBackground">Skyrocket</span> your client’s revenue and take all the glory with 100% discreet white label Google Ads services.
+            </h1>
             <br />
-            <p>Skyward’s hassle-free white label content writing services enable agencies to scale up profitably.</p>
+            <p>Skyward’s hassle-free white label Google Ads services enable agencies to scale up profitably.</p>
             <br />
-            <Button href={"/"}>See Our Plans & Pricing</Button>
+            <Button href={"/"}>EXPLORE PLANS & PRICING</Button>
+            <br />
+            <Image src={meta} alt="" style={{ width: "30%", border: "1px solid #000", borderRadius: "15px", marginTop: "40px", background: "#fff" }} />
           </div>{" "}
           <div className={classes.heroForm}>
             <div className={classes.form}>
-              <h3 className="title">Get a free audit</h3>
-              <p>100% Transparency, No BS, No dodgy stuff. Just pure results.</p>
+              <h3 className="title">Schedule A 20 Min. Free Discovery Call</h3>
+
               <form onSubmit={submitHandler}>
                 <input type="text" name="fullName" id="" placeholder="Full Name" required />
                 <input type="text" name="websiteUrl" id="" placeholder="Website URL" required />
@@ -95,7 +90,7 @@ const whitelabelcontent = () => {
       <Section
         title={
           <>
-            Trusted By <span className="titleBackground">500+ Clients</span> Across The World
+            Trusted By <span className="titleBackground">700+ Clients</span> Across The World
           </>
         }
         padding={"70px 0"}
@@ -105,15 +100,16 @@ const whitelabelcontent = () => {
           Supporting agencies from N. America and Europe to Asia and Australia in hundreds of industries and niches.
         </p>
         <div className={classes.partnerLogo}>
-          <Image src={clutch} alt="" style={{ padding: "20px" }} />
-          <Image src={trustpilot} alt="" style={{ padding: "20px" }} />
-          <Image src={goodfirms} alt="" style={{ padding: "20px" }} />
+          <Image src={meta} alt="" />
+          <Image src={microsoft} alt="" />
+          <Image src={tiktok} alt="" />
+          <Image src={google} alt="" />
         </div>
       </Section>
       <Section
         title={
           <>
-            Take your <span className="titleBackground">agency</span> to the next level. Score more wins with white label PPC.
+            Take your <span className="titleBackground">agency</span> to the next level. Score more wins with white label Google Ads.
           </>
         }
         padding={"70px 0"}
@@ -145,16 +141,20 @@ const whitelabelcontent = () => {
         </Button>
       </Section>
 
-      {/* <FlipBox
+      <Boxes
         items={flipboxItems}
         title={
           <>
-            Our seasoned team of <span className="titleBackground">SEO experts</span> can help you with
+            What We Can Do For <span className="titleBackground">You</span>
           </>
         }
         background={"#fff"}
-      /> */}
+        padding={"70px 0"}
+      />
 
+      <Section title={<span className="titleBackground">Pricing</span>} padding={"70px 0"}>
+        <Pricing />
+      </Section>
       <Section
         title={
           <>
@@ -165,7 +165,7 @@ const whitelabelcontent = () => {
         background={"#fff"}
       >
         <p style={{ width: "50%", margin: "50px auto", textAlign: "center", fontSize: "24px" }}>
-          Work with a fully-functional, fast-moving crew of content writers and copywriters that can move rapidly and achieve aggressive growth.
+        Work with a fully-functional, fast-moving group of PPC experts that can move rapidly and achieve aggressive growth.
         </p>
         <div className={classes.revenueGoals}>
           <Image src={australia} alt="" />
@@ -174,7 +174,6 @@ const whitelabelcontent = () => {
           See Results
         </Button>
       </Section>
-
       <Section
         padding={"70px 0"}
         title={
@@ -187,21 +186,22 @@ const whitelabelcontent = () => {
           <div className={classes.abc}>
             <h2>1</h2>
             <h3>Title</h3>
-            <p>
-              Our white label content writing services are designed to achieve authentic business growth. From onboarding to website launch, our web design reseller service is thoroughly scalable.
-            </p>
+            <p>Our white label PPC services are designed to achieve authentic business growth. From onboarding to campaign optimisations, our PPC reseller service  is thoroughly scalable.</p>
             <Image src={wlweb} alt="" />
           </div>
           <div className={classes.abc}>
             <h2>2</h2>
             <h3>Hassle free onboarding</h3>
-            <p>Speak to one of our onboarding superstars, fill in our questionnaire and you’re all set to go!</p>
+            <p>Speak to one of our Google Ads experts, fill in our onboarding questionnaire and you’re all set to go!
+</p>
             <Image src={wlweb} alt="" />
           </div>
           <div className={classes.abc}>
             <h2>3</h2>
             <h3>Results-driven strategy</h3>
-            <p>With extensive experience across a diverse range of clients in both size and industry, our team of PPC experts know how to optimize your clients’ campaigns to get the best results.</p>
+            <p>
+            With extensive experience across a diverse range of clients in both size and industry, our team of Google Ads experts know how to optimize your clients’ campaigns to get the best results.
+            </p>
             <Image src={wlweb} alt="" />
           </div>
         </div>
@@ -209,49 +209,7 @@ const whitelabelcontent = () => {
           See Results
         </Button>
       </Section>
-      <Section padding={"70px 0"} title={<span className="titleBackground">Our content writing process</span>} background={"#fff"}>
-        <div className={classes.abcContainer}>
-          <div className={classes.abc}>
-            <h2>1</h2>
-            <h3>Title</h3>
-            <p>Our reliable method for producing content writing that stirs readers and helps you achieve goals.</p>
-            <Image src={wlweb} alt="" />
-          </div>
-          <div className={classes.abc}>
-            <h2>2</h2>
-            <h3>Prepare</h3>
-            <p>We spend quality time understanding your goals and needs. We cultivate content strategy relevant to brand identity and desired outcomes. Preparation is where success begins. </p>
-            <Image src={wlweb} alt="" />
-          </div>
-          <div className={classes.abc}>
-            <h2>3</h2>
-            <h3>Lift off</h3>
-            <p>Let the work begin! This is where our skilled team uses our breadth of talent to start producing writing and content that will empower you.</p>
-            <Image src={wlweb} alt="" />
-          </div>
-          <div className={classes.abc}>
-            <h2>4</h2>
-            <h3>The Journey</h3>
-            <p>Content writing is a journey. That journey includes the production, promotion, and revision of content. We will be with you every step of the way.</p>
-            <Image src={wlweb} alt="" />
-          </div>
-          <div className={classes.abc}>
-            <h2>5</h2>
-            <h3>Land</h3>
-            <p>We spend time utilising our services to help your business land successfully. We specialise in strategies like SEO writing to achieve your desired results.</p>
-            <Image src={wlweb} alt="" />
-          </div>
-          <div className={classes.abc}>
-            <h2>6</h2>
-            <h3>Propel</h3>
-            <p>There is more to be done than just producing content. We take the finished product and propel it towards a wider reach and greater success.</p>
-            <Image src={wlweb} alt="" />
-          </div>
-        </div>
-        <Button href="/" position={"center"}>
-          See Results
-        </Button>
-      </Section>
+
       <Section
         title={
           <>
@@ -259,6 +217,7 @@ const whitelabelcontent = () => {
           </>
         }
         padding={"70px 0"}
+        background={"#fff"}
       >
         <p style={{ width: "50%", margin: "50px auto", textAlign: "center", fontSize: "24px" }}>Agencies face many difficult dilemmas, but partnering with us shouldn't be one of them.</p>
         <div className={classes.industryBoxes}>
@@ -294,33 +253,13 @@ const whitelabelcontent = () => {
           </div>
         </div>
         <Button href="/" position={"center"}>
-          Let's Talk
+          TALK TO US
         </Button>
       </Section>
-      <Section title={<span className="titleBackground">Pricing</span>} padding={"70px 0"} background={"#fff"}>
-        <Pricing />
-      </Section>
-      <Boxes
-        items={flipboxItems}
-        title={
-          <>
-            <span className="titleBackground">Content writing</span> that is sharp, concise and that communicates powerfully.
-          </>
-        }
-        desc={
-          <p className="subtitle">
-            Description: Writing is powerful if utilised correctly. We specialise in a universe of services including website content, blog writing, landing pages, resume writing, brand and
-            advertising, email promotions, newsletters, and press releases. We employ our own in-house content writing strategy to produce quality and powerful writing to propel your brand forward.
-            Whether it’s a website or an email promotion, our content writing will launch your desired message in a way that will land with your readers.
-          </p>
-        }
-        padding={"70px 0"}
-      >
-        <Button href="/">Let's Talk</Button>
-      </Boxes>
-      <Testimonials padding={"70px 0"} background={"#fff"} />
+      <Testimonials padding={"70px 0"} />
+      <CaseStudies background={"#f6f6f6"} />
     </React.Fragment>
   );
 };
 
-export default whitelabelcontent;
+export default whitelabelgoogle;
